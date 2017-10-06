eb32c5e7096db460e1c2419679da5c195245dfe2
Douglas Sellers <doug@fitorb.com>
2017-10-05 17:58:08 -0700

    Made it so that things are using the subrepo

--- js/CartItem.js
+++ js/CartItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import noop from './noop';
 
+
 export default class CartItem extends React.Component {
   constructor(props) {
     super(props);
