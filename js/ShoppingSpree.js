dd625923a3a16184660f477489120f6162662e43
Douglas Sellers <doug@fitorb.com>
2017-10-30 15:37:07 -0700

    Exit works again

--- js/ShoppingSpree.js
+++ js/ShoppingSpree.js
@@ -165,9 +165,10 @@ export default class ShoppingSpree extends FirebaseComponent {
   }
 
   doneShoppingSpree() {
-    this.cookies.remove('shopping_spree_name');
-    this.cookies.remove('shopping_spree_email');
-    this.cookies.remove('shopping_spree_id');
+    console.log( 'done with shopping spree' );
+    this.cookies.remove('shopping_spree_name', { path: '/', secure: false });
+    this.cookies.remove('shopping_spree_email', { path: '/', secure: false });
+    this.cookies.remove('shopping_spree_id', { path: '/', secure: false });
     this.setState(
       {
         display: 'none',
